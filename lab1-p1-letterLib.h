328b9c6752e5f9707e3977a5eabc3fab4c8a02ab
bbriceno <42550883+bbriceno@users.noreply.github.com>
2019-02-27 12:21:27 -0600

    Create lab1-p1-letterLib.h

---
+++ lab1-p1-letterLib.h
@@ -0,0 +1,11 @@
+//header files normally contain constants, global variables, #define directives, as well as 
+
+//function headers 
+
+
+
+//mostly function headers (prototypes, signatures) are listed in the header file
+
+
+
+void drawCharP(double ulCornerX, double ulCornerY, double side);
