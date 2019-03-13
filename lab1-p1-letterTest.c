72d2e9b560799512615c9b1a59992983591358c8
bbriceno <42550883+bbriceno@users.noreply.github.com>
2019-03-13 15:35:21 -0500

    Update lab1-p1-letterTest.c

--- lab1-p1-letterTest.c
+++ lab1-p1-letterTest.c
@@ -8,26 +8,49 @@ void drawCharP(double ulCornerX, double ulCornerY, double side)
 
     // calculate x,y coordinates of all corners
    double height = side;
-   double width = height*.75;
-   double ulx = width - width;
-   double uly = height - height;
-   double llx = width - width;
-   double lly = height;
-   double urx = width;
-   double ury = height - height;
-   double lrx = width;
-   double lry = height;
+   double ucornery = ulCornerY - ulCornerY;
+   double ucornerx = ulCornerX - ulCornerX;
+   double lcornery = ulCornerY;
+   double lcornerx = ulCornerX - ulCornerX;
+   double lrcornery = ulCornerY;
+   double lrcornerx = ulCornerX * 1/3;
+   double mmcornery = ulCornerY * 1/2;
+   double mmcornerx = ulCornerX * 1/3;
+   double mrcornery = ulCornerY * 1/2;
+   double mrcornerx = ulCornerX * 4/5;
+   double mlcornery = ulCornerY * 2/5; 
+   double mlcornerx = ulCornerX; 
+   double upcornery = ulCornerY *1/10; 
+   double upcornerx = ulCornerX; 
+   double trcornery = ulCornerY - ulCornerY; 
+   double trcornerx = ulCornerX * 4/5;
+   double ilcornery = ulCornerY * 1/10;
+   double ilcornerx = ulCornerX * 1/3;
+   double ibcornery = ulCornerY * 2/5;
+   double ibcornerx = ulCornerX * 1/3;
+   double ircornery = ulCornerY * 2/5;
+   double ircornerx = ulCornerX * .73;
+   double mbcornery = ulCornerY * 3/10;
+   double mbcornerx = ulCornerX * 4/5;
+   double mtcornery = ulCornerY * 1/5;
+   double mtcornerx = ulCornerX * 4/5;
+   double itcornery = ulCornerY * 1/10;
+   double itcornerx = ulCornerX * .73;
 
-   gfx_line(ulCornerX+width-width,ulCornerY+height-height,ulCornerX+.4*width,ulCornerY+.55*height);
-   gfx_line(ulCornerX+.4*width,ulCornerY+.55*height,ulCornerX+.4*width,ulCornerY+height);
-   gfx_line(ulCornerX+.4*width,ulCornerY+height,ulCornerX+.6*width,ulCornerY+height);
-   gfx_line(ulCornerX+.6*width,ulCornerY+height,ulCornerX+.6*width,ulCornerY+.55*height);
-   gfx_line(ulCornerX+.6*width,ulCornerY+.55*height,ulCornerX+urx,ulCornerY+ury);
-   gfx_line(ulCornerX+width,ulCornerY+height-height,ulCornerX+.8*width,ulCornerY+height-height);
-   gfx_line(ulCornerX+.8*width,ulCornerY+height-height,ulCornerX+.5*width,ulCornerY+.375*height);
-   gfx_line(ulCornerX+.5*width,ulCornerY+.375*height,ulCornerX+.2*width,ulCornerY+height-height);
-   gfx_line(ulCornerX+.2*width,ulCornerY+height-height,ulCornerX+width-width,ulCornerY+height-height);
-
+   gfx_line(ucornerx, ucornery, lcornerx, lcornery);
+   gfx_line(lcornerx, lcornery, lrcornerx, lrcornery); 
+   gfx_line(lrcornerx, lrcornery, mmcornerx, mmcornery);
+   gfx_line(mmcornerx, mmcornery, mrcornerx, mrcornery);
+   gfx_line(mrcornerx, mrcornery, mlcornerx, mlcornery);
+   gfx_line(mlcornerx, mlcornery, upcornerx, upcornery);
+   gfx_line(upcornerx, upcornery, trcornerx, trcornery);
+   gfx_line(ilcornerx, ilcornery, ibcornerx, ibcornery);
+   gfx_line(ibcornerx, ibcornery, ircornerx, ircornery);
+   gfx_line(ircornerx, ircornery, mbcornerx, mbcornery);  
+   gfx_line(mbcornerx, mbcornery, mtcornerx, mtcornery);
+   gfx_line(mtcornerx, mtcornery, mtcornerx, mtcornery);
+   gfx_line(mtcornerx, mtcornery, itcornerx, itcornery);
+   gfx_line(itcornerx, itcornery, ilcornerx, ilcornery);
 }
 
 	int main(int argc, char* argv[])
@@ -51,7 +74,7 @@ void drawCharP(double ulCornerX, double ulCornerY, double side)
 		gfx_color(0,250,0);
 		drawCharP(125,125,40);
 		gfx_color(100,100,100);
-		drawCharP(170,170,40);
+		drawCharP(100,75,100);
 		
 		
 		while(1) {
